7800f0cd5f43655ccfe65a2b05253316ce4ce1ff
Mikhail Poluboyarinov <mikle.sol@gmail.com>
2022-01-14 11:25:55 +0300

    bump version 0.3.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_macro"
 plugin_name = "Macro"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.0"
+plugin_version = "0.3.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
